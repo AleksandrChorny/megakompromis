2370ca3bf87f14f8357be40b13e5757e61802486
AleksandrChorny <99405017+AleksandrChorny@users.noreply.github.com>
2022-04-28 19:56:27 +0300

    Co-authored-by: Andrii Bondar <BondarAS@users.noreply.github.com>

--- src/js/modules/color-choice.js
+++ src/js/modules/color-choice.js
@@ -6,15 +6,18 @@ export function choiceColor(options) {
 
          if (event.target.closest(`.${options.actionClassName} `)) {
 
-            updGoodsImage(event);
+            if (event.target.closest(`.${options.goodsColor}`)) {
+               updGoodsImage(event);
+               return;
+            }
 
             event.target.closest(`.${options.goodsColorBoard}`).classList.remove(options.actionClassName);
 
-            event.target.closest(`.${options.goodsColorBoard}`)
-               .querySelectorAll(`.${options.goodsColor}`)
-               .forEach((element) => {
-                  element.classList.remove(options.actionClassName);
-               });
+            //event.target.closest(`.${options.goodsColorBoard}`)
+            //   .querySelectorAll(`.${options.goodsColor}`)
+            //   .forEach((element) => {
+            //      element.classList.remove(options.actionClassName);
+            //   });
             return;
          }
 
@@ -22,11 +25,11 @@ export function choiceColor(options) {
          event.target.closest(`.${options.goodsColorBoard}`).classList.add(options.actionClassName);
 
          //add _action to color item
-         event.target.closest(`.${options.goodsColorBoard}`)
-            .querySelectorAll(`.${options.goodsColor}`)
-            .forEach((element) => {
-               element.classList.add(options.actionClassName);
-            });
+         //event.target.closest(`.${options.goodsColorBoard}`)
+         //   .querySelectorAll(`.${options.goodsColor}`)
+         //   .forEach((element) => {
+         //      element.classList.add(options.actionClassName);
+         //   });
       }
    });
 
@@ -41,29 +44,5 @@ export function choiceColor(options) {
             .querySelector(`.${options.goodsPrice}`)
             .innerHTML = `Ціна: ${event.target.getAttribute("data-price")} грн.`
       }
-
    }
 }
-
-
-
-
-//const modal = document.querySelector(`.${ options.actionBlockClass } `);
-////Якщо неіснує класу модального вікна показуємо alert з повідомленням
-//if (event.target.closest(`.${ options.buttonToActionBlockClass } `) && !modal) {
-//   alert(`Модального вікна з класом ${ options.actionBlockClass } не існує`);
-//   return;
-//}
-////Додаємо клас та блокуємо скрол
-//if (event.target.closest(`.${ options.buttonToActionBlockClass } `) && modal) {
-//   modal.classList.add(options.actionClassName);
-//   if (document.querySelector("body").style.overflow !== "hidden" && options.scroll === false) {
-//      document.querySelector("body").style.overflow = "hidden";
-//   }
-//   return;
-//}
-////Знімаємо клас та деблокуємо скрол
-//if (event.target.closest(`.${ options.buttonToCloseBlockClass } `) || event.target.classList.contains(options.actionBlockClass)) {
-//   modal.classList.remove(options.actionClassName);
-//   document.querySelector("body").style.overflow = "";
-//}
