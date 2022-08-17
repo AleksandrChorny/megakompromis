665fe516a4b2e5c6b7cfe31a6e396b843ba21f70
Oleksandr <info@megakompromis.com>
2022-08-17 15:30:42 +0300

    new build withaut modal in js

--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,13 @@ import * as MK_Functions from "./modules/functions.js";
 MK_Functions.isWebp();
 MK_Functions.headerOnScroll();
 MK_Functions.onClickADD_action();
-MK_Functions.activateBlock({
-   actionBlockClass: 'modal',
-   buttonToActionBlockClass: 'heder__button',
-   buttonToCloseBlockClass: 'modal__close',
-   actionClassName: '_action',
-   scroll: false
-})
+//MK_Functions.activateBlock({
+//   actionBlockClass: 'modal',
+//   buttonToActionBlockClass: 'heder__button',
+//   buttonToCloseBlockClass: 'modal__close',
+//   actionClassName: '_action',
+//   scroll: false
+//})
 MK_Functions.choiceColor({
    goodsCard: 'goods-card',
    goodsColorBoard: 'goods-card__color-items',
